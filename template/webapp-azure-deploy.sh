a2286e698ca834988e15a3c968648804e4594436
kuritatu18 <kuritatu18@gmail.com>
2017-06-26 17:33:21 +0900

    Added or Updated deployment shell script

---
+++ template/webapp-azure-deploy.sh
@@ -0,0 +1,52 @@
+#!/bin/sh
+
+TARGET_RESOURCE_GROUP=user0010-webapp-tmpl-rg
+NUMBER_OF_WEB_SERVERS=3
+WEBSV_IMAGE="/subscriptions/50838fe3-59fa-4686-affc-34a1ba8df912/resourceGroups/user0010-webapp-images-rg/providers/Microsoft.Compute/images/webapp-websv-image"
+SSH_USER=webapusr
+SSH_PKEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCrJ5RPbD2JrMnv9ESGrkqU3SFwZq24R6a6KB0G0w7f3fCQxMJSga/pw5K+r6/VNj2NObCDJkY65GYtgdpDQUYtC0lUp6JiMJVVQ6sccZ0qFnL9mjLJ+95DKNmRRgmB4ziUJgypphB20oQTw15H9cns3EC9Wn3EBDiUpNxTtrFEaGCxvvX5M94mdRgNrsB+4OVcyvUVplOOjq5woJ0AzP3Ek0q4tq7AhIdHEq9pG+a3DB1pzbDXNqz9z2v89isJFvJdvbK/M5VVVZnb4Uru6GcQbI7yKq3podT6J039UtrCzt+n50TUJkdnj6OrDcbbB1EqcGTqpRMy+YnXtc/kBGJF devops"
+
+az configure --defaults group=${TARGET_RESOURCE_GROUP}
+az network nsg create -n webapp-websv-nsg
+az network nsg rule create \
+    --nsg-name webapp-websv-nsg -n webapp-websv-nsg-http \
+    --priority 1001 --protocol Tcp --destination-port-range 80
+az network public-ip create -n webapp-pip
+az network vnet create \
+    -n webapp-vnet --address-prefixes 192.168.1.0/24 \
+    --subnet-name webapp-vnet-sub --subnet-prefix 192.168.1.0/24
+az network lb create \
+    -n webapp-websv-lb --public-ip-address webapp-pip \
+    --frontend-ip-name webapp-websv-lb-front \
+    --backend-pool-name webapp-websv-lb-backpool
+az network lb probe create \
+    --lb-name webapp-websv-lb -n webapp-websv-lb-probe \
+    --port 80 --protocol Http --path '/?lbprobe=1'
+az network lb rule create \
+    --lb-name webapp-websv-lb -n webapp-websv-lb-rule \
+    --frontend-ip-name webapp-websv-lb-front --frontend-port 80 \
+    --backend-pool-name webapp-websv-lb-backpool --backend-port 80 \
+    --protocol tcp --probe-name webapp-websv-lb-probe
+az vm availability-set create -n webapp-websv-as \
+    --platform-update-domain-count 5 \
+    --platform-fault-domain-count 2
+for i in $(seq 1 ${NUMBER_OF_WEB_SERVERS}); do
+(
+az network nic create \
+    -n webapp-websv${i}-nic \
+    --private-ip-address 192.168.1.$((10 + ${i})) \
+    --vnet-name webapp-vnet --subnet webapp-vnet-sub \
+    --network-security-group webapp-websv-nsg \
+    --lb-name webapp-websv-lb \
+    --lb-address-pools webapp-websv-lb-backpool
+az vm create \
+    -n websv${i} --nics webapp-websv${i}-nic \
+    --availability-set webapp-websv-as \
+    --size Standard_F1 --storage-sku Standard_LRS \
+    --image ${WEBSV_IMAGE} \
+    --admin-username "${SSH_USER}" --ssh-key-value "${SSH_PKEY}"
+)&
+done
+wait
+echo http://$(az network public-ip show -n webapp-pip -o tsv --query ipAddress)/
+
